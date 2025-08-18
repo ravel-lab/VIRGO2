c65345e61a1fa2d2ad177eefa1bfaf626ca65a3d
michaelfrance <michael.t.france@gmail.com>
2025-08-18 12:41:03 -0400

    ongoing updates

--- AccessoryScripts/VIRGO2_add_annotations.py
+++ AccessoryScripts/VIRGO2_add_annotations.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sys
 import numpy as np
 from datetime import date
+import os
+
 
 #this script takes as input the summary.NR output file from VIRGO
 #adds to it the Taxa, KEGG, and Length information by merging with
@@ -18,32 +20,33 @@ data = pd.read_csv(sys.argv[1],sep="\t")
 #data.columns = ['PC']
 data['Gene'] = data['Gene'].astype('str')
 
+scriptLoc = os.path.realpath(__file__)
+scriptLoc = scriptLoc.split('VIRGO2_add_annotations.py')[0]
+
+
 #gene length data, every gene has a length
-len_data = pd.read_csv("~/bioinformatics_analysis/VIRGO2/08_annotation/0.VIRGO2.geneLength.txt",sep="\t",header=None)
+len_data = pd.read_csv("{}../AnnotationTables/0.VIRGO2.geneLength.txt".format(scriptLoc),sep="\t",header=None)
 len_data.columns = ['Gene','Length']
 len_data['Gene'] = len_data['Gene'].astype('str')
 
 #taxa annotation, not every gene has designation
-taxa_data = pd.read_csv("~/bioinformatics_analysis/VIRGO2/08_annotation/1.VIRGO2.taxon.txt",sep="\t")
-taxa_data = taxa_data.drop(['Cluster'],axis=1)
+taxa_data = pd.read_csv("{}../AnnotationTables/1.VIRGO2.taxon.txt".format(scriptLoc),sep="\t")
+taxa_data = taxa_data.drop(['Cluster','Cat'],axis=1)
 taxa_data['Gene'] = taxa_data['Gene'].astype('str')
 
-#kegg annotation, not every gene has designation
-#kegg_data = pd.read_csv("~/bioinformatics_tools/virgo/8.A.kegg.ortholog.txt",sep="\t",header=None)
-#kegg_data.columns = ['PC','KEGG','g1','g2']
-#kegg_data = kegg_data.drop(['g1','g2'],axis=1)
-#kegg_data['PC'] = kegg_data['PC'].astype('str')
+#kegg annotation, not every gene has a designation
+kegg_data = pd.read_csv("{}../AnnotationTables/7.VIRGO2.kegg.txt".format(scriptLoc),sep="\t")
+kegg_data = kegg_data[['Gene','KEGG']]
+kegg_data['Gene'] = kegg_data['Gene'].astype('str')
 
 #geneProduct annotation, not every gene has annotation
-gp_data = pd.read_csv("~/bioinformatics_analysis/VIRGO2/08_annotation/6.VIRGO2.geneProduct.txt",sep="\t")
-#gp_data.columns = ['Cluster','PC','GeneProduct']
-#gp_data = gp_data.drop(['Cluster'],axis=1)
+gp_data = pd.read_csv("{}../AnnotationTables/6.VIRGO2.geneProduct.txt".format(scriptLoc),sep="\t")
 gp_data['Gene'] = gp_data['Gene'].astype('str')
 
 
 data = pd.merge(left=len_data,right=data,left_on="Gene",right_on='Gene',how='right')
 data = pd.merge(left=taxa_data,right=data,left_on="Gene",right_on='Gene',how='right')
-#data = pd.merge(left=kegg_data,right=data,on="Gene",how='right')
+data = pd.merge(left=kegg_data,right=data,on="Gene",how='right')
 data = pd.merge(left=gp_data,right=data,on="Gene",how='right')
 
 data.to_csv("%s_NR_anno_%s.csv" %(prefix,today),sep=",",index=None)
--- VIRGO2.py
+++ VIRGO2.py
@@ -6,6 +6,8 @@
 ###bowtie2
 ###samtools
 
+###V1 081825
+
 """
 Copyright 2023
 
@@ -50,7 +52,10 @@ except:
     print("Required package os not available")
     exit()
 
-VIRGO2DBLoc = '/local/scratch/mfrance/VIRGO2/12_package/VIRGO2'
+scriptLoc = os.path.realpath(__file__)
+scriptLoc = scriptLoc.split('VIRGO2.py')[0]
+
+VIRGO2DB = '{}/Index/VIRGO2'.format(scriptLoc)
 
 ###Defining function which corrects mapping output based on percent of gene covered
 def covCorr(samFile,coverageFile):
@@ -103,30 +108,74 @@ parser = argparse.ArgumentParser(description="VIRGO2 is a tool and associated da
 subparsers = parser.add_subparsers(dest='command')
 
 ###Viable commands
+#install -> unzip annotation and fasta files and build bowtie2 index
 #map -> map reads to VIRGO2 using bowtie2 and convert to gene counts
 #compile -> compile mapping outputs from a directory of mapping results
+#taxonomy -> produce taxonomic composition estimates from compiled VIRGO2 output
 #license -> print license
 
 subparsers.required = True
-#  subparser for mapping routine
+
+#subparser for install command
+parser_install = subparsers.add_parser('install')
+parser_install.add_argument('-p','--threads',help='Number of threads used in building index default:1',default='1',required=False)
+
+
+#subparser for mapping routine
 parser_map = subparsers.add_parser('map')
 
 # adding arguments for mapping routine
 parser_map.add_argument('-r','--reads',help='Single-End reads file, can be gzipped',required=True)
 parser_map.add_argument('-c','--cov',choices=['0','1'],help='Assign multi-mapped reads to gene with highest percent covered, 0=No,1=Yes, default:Yes',default='1',required=False)
-parser_map.add_argument('-p','--threads',help='Number of threads used in mapping default:1',default=1,required=False)
+parser_map.add_argument('-p','--threads',help='Number of threads used in mapping default:1',default='1',required=False)
 parser_map.add_argument('-o','--outputPrefix',help='Prefix used in the filename for the mapping output',required=True)
-parser_map.add_argument('-b','--bypass',choices=['0','1'],help='Sam and coverager files already generated, proceed to coverage correction directly 0=No, 1=Yes, default No',default='0',required=False)
+parser_map.add_argument('-b','--bypass',choices=['0','1'],help='Sam and coverage files already generated, proceed to coverage correction directly 0=No, 1=Yes, default No',default='0',required=False)
 #subparser for compile routine
 parser_compile = subparsers.add_parser('compile')
 # adding arguments for compile routine
-parser_compile.add_argument('-i','--input',help='Directory where bowtie2 mapping results are located')
+parser_compile.add_argument('-i','--input',help='Full path to directory where bowtie2 mapping results are located')
 parser_compile.add_argument('-o','--outputPrefix',help='Prefix used in the filename for the compiled output')
+
+parser_taxonomy = subparsers.add_parser('taxonomy')
+parser_taxonomy.add_argument('-i','--input',help='Full path to compiled results file')
+parser_taxonomy.add_argument('-o','--outputPrefix',help='Prefix used in the filename for the compiled output')
+parser_taxonomy.add_argument('-b','--bacteria',choices=['0','1'],help='Report composition including only the bacteria, default=1',default='1',required=False)
+parser_taxonomy.add_argument('-f','--filter',choices=['0','1'],help='Mask contribution from taxa where number of genes detected is below threshold, default=1',default='1',required=False)
+parser_taxonomy.add_argument('-r','--readCounts',choices=['0','1'],help='Report values as read counts instead of relative abundances, default=0',default='0',required=False)
+parser_taxonomy.add_argument('-m','--multigenera',choices=['0','1'],help="Report relative abundance of multigenera genes, off by default (0)",default='0',required=False)
 
 #subparser for license routine
 parser_license = subparsers.add_parser('license')
 
 args = parser.parse_args()
+
+if args.command == 'install':
+
+    for outFile in sorted(os.listdir("{}FastaFiles/".format(scriptLoc))):
+        print(outFile)
+        try:
+            if outFile.split('.')[-1]=='gz':
+                subprocess.run(["gunzip","{path}FastaFiles/{file}".format(path=scriptLoc,file=outFile)])
+            else:
+                pass    
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+
+    for outFile in sorted(os.listdir("{}AnnotationTables/".format(scriptLoc))):
+        print(outFile)
+        try:
+            if outFile.split('.')[-1]=='gz':
+                subprocess.run(["gunzip","{path}AnnotationTables/{file}".format(path=scriptLoc,file=outFile)])
+            else:
+                pass    
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+
+    try:
+        subprocess.run(["bowtie2-build","-f","--seed","343","--threads",args.threads,"{path}FastaFiles/VIRGO2.fa".format(path=scriptLoc),"{}Index/VIRGO2".format(scriptLoc)])
+    except subprocess.CalledProcessError as e:
+            print(e.output)
+
 
 #call functions for mapping
 if args.command == 'map':
@@ -136,7 +185,7 @@ if args.command == 'map':
         if args.bypass=='0':
             try:
                 subprocess.run(["bowtie2","-N","0","-L","20","-D","20","-R","3","-k","10","--sam-no-qname-trunc","--end-to-end","--seed","343",
-                        "--no-unal","-p",args.threads,"-x",str(VIRGO2DBLoc),"-U",args.reads,"-S","{}.sam".format(args.outputPrefix)])
+                        "--no-unal","-p",args.threads,"-x",str(VIRGO2DB),"-U",args.reads,"-S","{}.sam".format(args.outputPrefix)])
             except subprocess.CalledProcessError as e:
                 print(e.output)
 
@@ -176,7 +225,7 @@ if args.command == 'map':
         #mapping without coverage correction, WARNING inflates gene counts
         try:
             subprocess.run(["bowtie2","-N","0","-L","20","-D","20","-R","3","--sam-no-qname-trunc","--end-to-end","--seed","343",
-                        "--no-unal","-p",args.threads,"-x",str(VIRGO2DBLoc),"-U",args.reads,"-S","{}.sam".format(args.outputPrefix)])
+                        "--no-unal","-p",args.threads,"-x",str(VIRGO2DB),"-U",args.reads,"-S","{}.sam".format(args.outputPrefix)])
         except subprocess.CalledProcessError as e:
             print(e.output)
 
@@ -208,7 +257,7 @@ if args.command == 'compile':
         print(outFile)
         try:
             if outFile.split('.')[-1]=='out':
-                mappingDF = pd.read_csv(outFile,sep='\t',index_col=0)
+                mappingDF = pd.read_csv("{path}/{file}".format(path=str(args.input),file=str(outFile)),sep='\t',index_col=0)
                 mappingDF.columns = [outFile.split('.')[0]]
                 output2concat.append(mappingDF)
         except:
@@ -220,5 +269,93 @@ if args.command == 'compile':
     except:
         print('No VIRGO2 mapping output files to compile in the designated directory')
 
+if args.command == 'taxonomy':
+
+    print(args.input)
+    
+    #####reading in compiled output
+    try:
+        VIRGO2table = pd.read_csv(args.input,sep='\t')
+        VIRGO2table['Gene']=VIRGO2table['Gene'].astype(str)
+    except:
+        print('No compiled VIRGO2 output provided')
+
+    #####reading in annotation tables
+    try:
+        lenKey = pd.read_csv("{}/AnnotationTables/0.VIRGO2.geneLength.txt".format(scriptLoc),sep="\t")
+        lenKey['Gene'] = lenKey['Gene'].astype('str')
+    except:
+        print("Could not read the gene length annotation table (0.VIRGO2.geneLength.txt)")
+    #taxa annotation, not every gene has designation
+    try:
+        taxKey = pd.read_csv("{}/AnnotationTables/1.VIRGO2.taxon.txt".format(scriptLoc),sep="\t")
+        taxKey['Gene'] = taxKey['Gene'].astype('str')
+        taxKey = taxKey.drop(['Cluster','Cat'],axis=1)
+    except:
+        print("Could not read the taxon annotation table (1.VIRGO2.taxon.txt)")
+
+    VIRGO2table = pd.merge(left=lenKey,right=VIRGO2table,left_on="Gene",right_on='Gene',how='right')
+    VIRGO2table = pd.merge(left=taxKey,right=VIRGO2table,left_on="Gene",right_on='Gene',how='right')
+
+    try:
+        thresh = pd.read_csv("{}/AnnotationTables/2.VIRGO2.taxonThresholds.txt".format(scriptLoc),sep="\t",index_col=0)
+        bacteriaList=list(thresh[thresh['Domain']=='Bacteria'].index)
+        thresh = thresh.drop(['Domain','MedianGenes'],axis=1)
+        thresh = thresh[thresh['Thresh']>0]
+    except:
+        print("Could not read taxon threshold file (2.VIRGO2.taxonThresholds.txt)")
+    
+    #removes non-bacterial taxa by default
+    if args.bacteria=='1':
+        VIRGO2table=VIRGO2table[VIRGO2table['Taxa'].isin(bacteriaList)]
+    elif args.bacteria=='0':
+        pass
+
+
+    #counting number of genes detected per species per metagenome
+    VIRGO2bool = pd.concat([VIRGO2table[VIRGO2table.columns[1:2]],VIRGO2table[VIRGO2table.columns[3:]].astype(bool)],axis=1).groupby(by='Taxa').sum()
+    VIRGO2bool = pd.merge(left=thresh,right=VIRGO2bool,left_index=True,right_index=True,how="inner")
+    VIRGO2bool = VIRGO2bool[VIRGO2bool.columns[1:]].ge(VIRGO2bool['Thresh'],axis=0)
+
+    VIRGO2table = VIRGO2table.drop(['Gene'],axis=1)
+    
+
+    if args.multigenera=='0':
+        VIRGO2table = VIRGO2table[VIRGO2table['Taxa']!='MultiGenera']
+        VIRGO2bool=VIRGO2bool.drop(['MultiGenera'],axis=0)
+    elif args.multigenera=='1':
+        pass
+
+    VIRGO2table = VIRGO2table[VIRGO2table['Taxa'].notna()]
+
+    VIRGO2table = VIRGO2table.groupby("Taxa").sum()
+    VIRGO2table = VIRGO2table.reset_index()
+    VIRGO2table = VIRGO2table[VIRGO2table['Taxa'].isin(VIRGO2bool.index)]
+    VIRGO2table = VIRGO2table.drop(['Length'],axis=1)
+
+    #filtering data by detected taxa if requested, happens by default
+    if args.filter=='1':
+        VIRGO2table = VIRGO2table[VIRGO2table['Taxa'].isin(VIRGO2bool.index)]
+        VIRGO2table = VIRGO2table.set_index("Taxa")
+        TaxaTableOut = VIRGO2table*VIRGO2bool
+    elif args.filter=='0':
+        TaxaTableOut = VIRGO2table.set_index("Taxa")
+    #calculating relative abundance, if requested, happens by default
+
+    if args.readCounts=='0':
+        TaxaTableOut=TaxaTableOut.div(TaxaTableOut.sum(axis=0),axis=1)
+        reportType='relAbund'
+    elif args.readCounts=='1':
+        TaxaTableOut=TaxaTableOut
+        reportType='readCount'
+
+    #sorting the output taxa by average relative abundance across all samples
+    TaxaTableOut['taxa_ave_abundance'] = TaxaTableOut.sum(axis=1)
+    TaxaTableOut = TaxaTableOut.sort_values(by=["taxa_ave_abundance"],ascending=False)
+    TaxaTableOut = TaxaTableOut.drop(['taxa_ave_abundance'],axis=1)
+    
+    TaxaTableOut = TaxaTableOut.T
+    TaxaTableOut.to_csv("{}.{}.csv".format(args.outputPrefix,reportType),sep=",")
+
 if args.command == 'license':
-    subprocess.run(['cat','License.txt'])
+    subprocess.run(['cat','{}/LICENSE'.format(scriptLoc)])
